85db4b46f4d3b9bc7b23599c153457ada58cb519
Olga Zatulivetrova <zatulivetrova@gmail.com>
2022-08-18 15:53:47 +0300

    Delete Autouse, added params to skip fixture

--- test_parametrize.py
+++ test_parametrize.py
@@ -3,7 +3,7 @@ from selene import have
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='function', autouse=True, params=[(1980, 1080)])
+@pytest.fixture(scope='function', params=[(1980, 1080)])
 def browser_desktop(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
@@ -15,7 +15,7 @@ def test_github_desktop(browser_desktop):
     browser.element('.auth-form-header').should(have.text("Sign in to GitHub"))
 
 
-@pytest.fixture(scope='function', autouse=True, params=[(337, 667)])
+@pytest.fixture(scope='function', params=[(337, 667)])
 def browser_mobile(request):
     browser.config.window_width = request.param[0]
     browser.config.window_height = request.param[1]
--- test_skip.py
+++ test_skip.py
@@ -4,25 +4,37 @@ import pytest
 from selene import have
 from selene.support.shared import browser
 
+chrome = pytest.fixture(params=[(1012, 946), (1200, 1000), (1011, 946), (900, 950)])
+
+
+@chrome
+def browser_size(request):
+    return request
+
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_config():
-    browser.config.window_width = 1980
-    browser.config.window_height = 1080
+def browser_config(browser_size):
+    width = browser_size.param[0]
+    height = browser_size.param[1]
 
+    browser.config.window_width = width
+    browser.config.window_height = height
 
-def test_github_desktop(browser_config):
-    if browser.config.window_width < 1000:
-        pytest.skip('Window width is for mobile test')
     browser.open("https://github.com/")
+
+
+def test_github_desktop():
+    if browser._config.window_width < 1012:
+        pytest.skip('Window width is for mobile test')
+
     browser.element("a.flex-shrink-0:nth-child(1)").click()
     browser.element('.auth-form-header').should(have.text("Sign in to GitHub"))
 
 
-def test_github_mobile(browser_config):
-    if browser.config.window_width > 1000:
+def test_github_mobile():
+    if browser._config.window_width > 1011:
         pytest.skip('Window width is for desktop test')
-    browser.open("https://github.com/")
+
     browser.element('button[aria-label="Toggle navigation"').click()
     browser.element('a[href="/login"]').click()
     browser.element('.auth-form-header').should(have.text("Sign in to GitHub"))
